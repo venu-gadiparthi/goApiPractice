5c63b3ba311aa575d3900adc27c2d4ec8e9d9e0e
Venu G <venu-gadiparthi@outlook.com>
2024-09-28 21:25:59 -0700

    NewRequest working

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 )
 
@@ -18,9 +19,22 @@ func main() {
 	plan, _ := os.Open("configuration.json")
 	byteValue, _ := io.ReadAll(plan)
 	var dats Data
-
-	// Unmarshal the byte array into the 'person' variable.
 	json.Unmarshal(byteValue, &dats)
 	fmt.Println(dats.Token)
+	usersUrl := "https://trial-3926351.okta.com/api/v1/users"
+	resp, _ := http.NewRequest(http.MethodGet, usersUrl, nil)
+	resp.Header.Set("Authorization", dats.Token)
+	outs, err := http.DefaultClient.Do(resp)
+	if err != nil {
+		fmt.Println(err, "er one")
+	}
+	body, err1 := io.ReadAll(outs.Body)
+	if err1 != nil {
+		fmt.Println(err1, "er two")
+	}
+	bodyString := string(body)
+	fmt.Println(bodyString)
+
+	// Unmarshal the byte array into the 'person' variable.
 
 }
