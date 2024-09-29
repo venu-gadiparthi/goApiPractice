afdef58f33101b383cb087235b9e136e64ff83b0
Venu G <venu-gadiparthi@outlook.com>
2024-09-28 23:07:40 -0700

    dsf'

--- main.go
+++ main.go
@@ -8,31 +8,35 @@ import (
 	"os"
 )
 
-type Data struct {
-	Token string `json:"token"`
+type Configuration struct {
+	Token  string `json:"token"`
+	Domain string `json:"domain"`
+	Users  string `json:"users"`
 }
 
 func main() {
+	var config Configuration
 	plan, _ := os.Open("configuration.json")
 	byteValue, _ := io.ReadAll(plan)
-	var dats Data
-	json.Unmarshal(byteValue, &dats)
-	fmt.Println(dats.Token)
-	usersUrl := "https://trial-3926351.okta.com/api/v1/users"
+	json.Unmarshal(byteValue, &config)
+	usersUrl := config.Domain + config.Users
+
 	resp, _ := http.NewRequest(http.MethodGet, usersUrl, nil)
-	resp.Header.Set("Authorization", dats.Token)
+	resp.Header.Set("Authorization", config.Token)
 	outs, err := http.DefaultClient.Do(resp)
+
 	if err != nil {
-		fmt.Println(err, "er one")
+		fmt.Println(err)
 	}
 	body, err1 := io.ReadAll(outs.Body)
 	if err1 != nil {
-		fmt.Println(err1, "er two")
+		fmt.Println(err1)
 	}
+
 	var v []map[string]interface{}
-	err3 := json.Unmarshal(body, &v)
-	fmt.Println(err3)
-	for k, val := range v[0] {
-		fmt.Println(k, " - ", val)
-	}
+	err = json.Unmarshal(body, &v)
+
+	// for k, val := range v[0] {
+	// 	fmt.Println(k, " - ", val)
+	// }
 }
